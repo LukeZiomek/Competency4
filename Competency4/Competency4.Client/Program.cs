0f999eb9d0da80349630908839421015d14282f4
LUKE ZIOMEK <lziomek@cnm.edu>
2024-11-22 03:53:21 -0700

    Add project files.

---
+++ Competency4/Competency4.Client/Program.cs
@@ -0,0 +1,20 @@
+using Competency4.Client;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace Competency4.Client
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddCascadingAuthenticationState();
+            builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
